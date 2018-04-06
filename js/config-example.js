c981c8d89b1fc4a357779d3d0af4d746d341321b
Romain Laneuville <romain.laneuville@hotmail.fr>
2018-04-06 10:32:29 +0200

    update config.js

--- js/config-example.js
+++ js/config-example.js
@@ -4,12 +4,12 @@ export default {
         "loginPage"    : "login",
         "homePage"     : "home",
         "pageSelectors": {
-            "currentPage"       : ".current-page",
-            "page"              : ".page",
-            "pageLink"          : ".page-link",
-            "pageLinkParameters": ".page-link-parameters"
+            "currentPage": ".current-page",
+            "page"       : ".page",
+            "pageLink"   : ".page-link"
         },
         "menuSelectors": {
+            "menu"        : "#menu-left",
             "buttonToggle": "#toggle-menu-left",
             "cover"       : "#menu-left .cover",
             "name"        : "#user-view .name",
@@ -19,79 +19,9 @@ export default {
         "urlPrefix"    : "#!"
     },
     "ejs"         : {
-        "selectors": {
+        "chipsDataSeparator": "||",
+        "selectors"         : {
             "loader": ".preloader-wrapper-container"
-        }
-    },
-    "flow"        : {
-        "tree"     : {
-            "container": "#flow-container",
-            "width"    : 500,
-            "height"   : 500,
-            "node"     : {
-                "width"        : 200,
-                "height"       : 100,
-                "paddingTop"   : 30,
-                "paddingBottom": 200,
-                "paddingRight" : 20,
-                "paddingLeft"  : 20,
-                "stroke"       : "black",
-                "strokeWidth"  : 4,
-                "title"        : {
-                    "fontSize"  : "25",
-                    "fontFamily": "Calibri",
-                    "fill"      : "black"
-                }
-            },
-            "link"     : {
-                "pointerLength": 20,
-                "pointerWidth" : 20,
-                "fill"         : "black",
-                "stroke"       : "black",
-                "strokeWidth"  : 4
-            }
-        },
-        "selectors": {
-            "userInteraction": {
-                "addForm"              : "#add-user-interaction-form",
-                "save"                 : "#save-user-interaction-form",
-                "cancel"               : "#cancel-user-interaction-form",
-                "formContainer"        : "#add-user-interaction-form-container",
-                "responseTypeInput"    : "#user-interaction-accept-response-type-input",
-                "responseTypeContainer": "#user-interaction-accept-response-type-container",
-                "bindInput"            : "#user-interaction-bind-input",
-                "labelInput"           : "#user-interaction-label-input",
-                "predefinedInput"      : "#predefined-user-interaction-input"
-            },
-            "botInteraction" : {
-                "addForm"      : "#add-bot-interaction-form",
-                "save"         : "#save-bot-interaction-form",
-                "cancel"       : "#cancel-bot-interaction-form",
-                "formContainer": "#add-bot-interaction-form-container",
-                "bindInput"    : "#bot-interaction-bind-input",
-                "labelInput"   : "#bot-interaction-label-input"
-            },
-            "addFormAction"  : {
-                "container": "#add-forms-action"
-            }
-        }
-    },
-    "bot"         : {
-        "selectors": {
-            "pages"  : {
-                "container": "#bot-pages-selector-container",
-                "input"    : "#bot-pages-selector-input"
-            },
-            "events" : {
-                "container": "#bot-events-selector-container",
-                "input"    : "#bot-events-selector-input"
-            },
-            "message": {
-                "container": "#bot-message-input-container",
-                "input"    : "#bot-message-input"
-            },
-            "save"   : "#save-bot-edition",
-            "cancel" : "#cancel-bot-edition"
         }
     },
     "home"        : {
@@ -99,6 +29,24 @@ export default {
             "toggleBot": ".toggle-bot",
             "editBot"  : ".edit-bot",
             "removeBot": ".remove-bot"
+        }
+    },
+    "notesList"        : {
+        "pageContainer": "#notes-list-page-container",
+        "selectors"    : {
+            "notesContainer": "#notes-container",
+            "noteContainer" : ".card",
+            "noteFormBtn"   : "#note-form-open",
+            "notesListBtn"  : "#notes-list-open",
+            "saveNote"      : "#save-note",
+            "pinNote"       : ".pin",
+            "editNote"      : ".edit",
+            "removeNote"    : ".remove",
+            "form"          : {
+                "container": "#note-form",
+                "title"    : "#note-title-input",
+                "content"  : "#note-content-input",
+            }
         }
     },
     "notification": {
@@ -121,18 +69,35 @@ export default {
         "defaultLevel": "info"
     },
     "facebook"    : {
-        "appId"           : "140195826556349",
-        "apiRootUrl"      : "https://graph.facebook.com/v2.9",
-        "siteRootUrl"     : "https://facebook.com/v2.9",
-        "redirectionUrl"  : "http://localhost:8000",
-        "localCookieName" : "facebook_token",
-        "connectBtnId"    : "#facebook-connect",
-        "disconnectBtnId" : "#facebook-disconnect",
-        "authorizations"  : ["public_profile", "email", "manage_pages", "pages_messaging"],
-        "requiredUserInfo": ["id", "email", "name", "first_name", "last_name", "cover", "picture"]
+        "appId"                  : "468978316851998",
+        "apiRootUrl"             : "https://graph.facebook.com/v2.12",
+        "siteRootUrl"            : "https://facebook.com/v2.12",
+        "redirectionUrl"         : "https://todo-list.app",
+        "facebookTokenCookieName": "facebook_token",
+        "connectBtnId"           : "#facebook-connect",
+        "disconnectBtnId"        : "#facebook-disconnect",
+        "authorizations"         : ["public_profile", "email"],
+        "requiredUserInfo"       : [
+            "id",
+            "cover",
+            "email",
+            "name",
+            "first_name",
+            "last_name",
+            "age_range",
+            "link",
+            "gender",
+            "locale",
+            "picture",
+            "timezone",
+            "updated_time",
+            "verified"
+        ]
     },
     "app"         : {
-        "url"          : "http://facebook-api.app",
-        "personalToken": "artsmartiaux"
+        "url"                : "https://todo-list-api.app",
+        "userTokenCookieName": "user_token",
+        "userEmailCookieName": "user_email",
+        "personalToken"      : "artsmartiaux"
     }
 };
